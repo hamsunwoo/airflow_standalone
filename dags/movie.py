26854f239ad6f5c6c131111e758fe3fd194cd860
sunwoo <summerham22@gmail.com>
2024-07-29 16:44:48 +0900

    python op

--- dags/movie.py
+++ dags/movie.py
@@ -2,11 +2,13 @@ from datetime import datetime, timedelta
 from textwrap import dedent
 import subprocess
 import os
+from pprint import pprint
 
 from airflow import DAG
 
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
 
 os.environ['LC_ALL'] = 'C'
 
@@ -24,12 +26,38 @@ with DAG(
     tags=['movie'],
 ) as dag:
 
+    def get_data(ds, **kwargs):
+        print(ds)
+        print(args)
+        print("=" * 20)
+        print(f"ds_nodash => {kwargs['ds_nodash']}")
+        print(f"kwargs type => {type(kwargs)}")
+        print("=" * 20)
+        from  mov.api.call import get_key, save2df
+        key = get_key()
+        print(f"MOVIE_API_KEY=>{key}")
+        YYYYMMDD = kwargs['ds_nodash'] #20240724
+        df = save2df(YYYYMMDD)
+        print(df.head(5))
+
+    def print_context(ds=None, **kwargs):
+        """Print the Airflow context and ds variable from the context."""
+        print("::group::All kwargs")
+        pprint(kwargs)
+        print("::endgroup::")
+        print("::group::Context variable ds")
+        print(ds)
+        print("::endgroup::")
+        return "Whatever you return gets printed in the logs"
+
+    run_this = PythonOperator(
+            task_id="print_the_context",
+            python_callable=print_context
+            )
        
-    task_get_data = BashOperator(
+    task_get_data = PythonOperator(
             task_id="get.data",
-            bash_command="""
-                echo "get data"
-            """
+            python_callable=get_data #함수이름
             )
     
     task_save_data = BashOperator(
@@ -43,5 +71,5 @@ with DAG(
     task_start = EmptyOperator(task_id='start')
     task_end = EmptyOperator(task_id='end',trigger_rule="all_done")
 
-    task_start >> task_get_data >> task_save_data
-    task_save_data >> task_end
+    task_start >> task_get_data >> task_save_data >> task_end
+    task_start >> run_this >> task_end
