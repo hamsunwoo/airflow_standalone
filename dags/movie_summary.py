3caee005c8ea3d48d6befd1128230bee4f10ac57
sunwoo <summerham22@gmail.com>
2024-08-01 09:43:19 +0900

    update summery_movie

--- dags/movie_summary.py
+++ dags/movie_summary.py
@@ -19,7 +19,7 @@ from airflow.operators.python import (
 os.environ['LC_ALL'] = 'C'
 
 with DAG(
-     'movie',
+     'summery_movie',
     default_args={
         'depends_on_past': False,
         'retries': 1,
@@ -31,7 +31,7 @@ with DAG(
     schedule="10 2 * * *",
     start_date=datetime(2024, 7, 24),
     catchup=True,
-    tags=['movie'],
+    tags=['summery'],
 ) as dag:
 
     def get_data(ds_nodash):
@@ -76,97 +76,26 @@ with DAG(
             return "get.start", "echo.task"
 
 
-    branch_op = BranchPythonOperator(
-            task_id="branch.op",
-            python_callable=branch_func
+    apply_type = EmptyOperator(
+            task_id="apply.type"
             )
     
-    save_data = PythonVirtualenvOperator(
-            task_id="save.data",
-            python_callable=save_data,
-            requirements=["git+https://github.com/hamsunwoo/movie.git@0.3/api"],
-            system_site_packages=False,
-            trigger_rule='one_success',
-            #venv_cache_path="tmp2/airflow_venv/get_data"
+    merge_df = EmptyOperator(
+            task_id="merge.df"
             )
 
-    #다양성 영화 유무 
-    multi_y = PythonVirtualenvOperator(
-            task_id='multi.y',
-            python_callable=func_multi,
-            op_args=["{{ds_nodash}}"],
-            op_kwargs={
-                'url_param': {"multiMovieYn": "Y"}
-                       },
-            requirements=["git+https://github.com/hamsunwoo/movie.git@0.3/api"],
-            system_site_packages=False
-            )
+    de_dup = EmptyOperator(
+                task_id='de.dup'
+             )
 
-    multi_n = PythonVirtualenvOperator(
-            task_id='multi.n',
-            python_callable=func_multi,
-            op_args=["{{ds_nodash}}"],
-            op_kwargs={
-                'url_param': {"multiMovieYn": "N"}
-                    },
-            requirements=["git+https://github.com/hamsunwoo/movie.git@0.3/api"],
-            system_site_packages=False
-            )
-
-    #한국외국영화
-    nation_k = PythonVirtualenvOperator(
-            task_id='nation.k',
-            python_callable=func_multi,
-            op_args=["{{ds_nodash}}"],
-            op_kwargs={
-                'url_param': {"repNationCd": "K"}
-                    },
-            requirements=["git+https://github.com/hamsunwoo/movie.git@0.3/api"],
-            system_site_packages=False
-            )
-
-    nation_f = PythonVirtualenvOperator(
-            task_id='nation.f',
-            python_callable=func_multi,
-            op_args=["{{ds_nodash}}"],
-            op_kwargs={
-                'url_param': {"repNationCd": "F"}
-                    },
-            requirements=["git+https://github.com/hamsunwoo/movie.git@0.3/api"],
-            system_site_packages=False
-            )
-
-    rm_dir = BashOperator(
-        task_id='rm.dir',
-        bash_command='rm -rf ~/tmp/test_parquet/load_dt/load_dt={{ ds_nodash }}'
-         )
-
-    echo_task = BashOperator(
-                task_id='echo.task',
-                bash_command="echo 'task'",
-                trigger_rule='all_success'
-                )
+    summery_df = EmptyOperator(
+                task_id='summery.df'
+                 )
         
 
     task_start = EmptyOperator(task_id='start')
     task_end = EmptyOperator(task_id='end')
 
-    get_start = EmptyOperator(task_id='get.start', trigger_rule='all_done')
-    get_end = EmptyOperator(task_id='get.end')
+    task_start >> apply_type >> merge_df
+    merge_df >> de_dup >> summery_df
 
-    throw_err = BashOperator(
-            task_id='throw.err',
-            bash_command="exit 1",
-            trigger_rule="all_done"
-            )
-    
-    task_start >> branch_op
-    task_start >> throw_err >> save_data
-
-    branch_op >> rm_dir >> get_start 
-    branch_op >> get_start
-    branch_op >> echo_task
-
-    get_start >> [multi_y, multi_n, nation_k, nation_f] >> get_end
-
-    get_end >> save_data >> task_end
